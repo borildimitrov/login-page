3607a5df520540fbf347b0ecdb0b306eed31e0dd
Boril Dimitrov <borild@MacBook-Pro.local>
2020-10-14 17:00:05 +0300

    Changed Auth/components -> Pages/

--- src/app/pages/auth.module.ts
+++ src/app/pages/auth.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './componenets/login/login.component';
-import { RegisterComponent } from './componenets/register/register.component';
-import { ResetPasswordComponent } from './componenets/reset-password/reset-password.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 
 @NgModule({
