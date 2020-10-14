7e066d65d6ad165b38aecceabc845d2807fc10c6
Boril Dimitrov <borild@MacBook-Pro.local>
2020-10-14 17:16:30 +0300

    Added margin to all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './pages/auth.module';
 
 
 @NgModule({
