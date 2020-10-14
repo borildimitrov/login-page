bfb89cc3ae1a2cd48387cdede429085c25f63dfc
Boril Dimitrov <borild@MacBook-Pro.local>
2020-10-14 17:19:52 +0300

    Changed auth routing module to pages routing module (just a rename)

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
+import { AuthRoutingModule } from './pages-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ResetPasswordComponent } from './forgot-password/reset-password.component';
